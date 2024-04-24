de4f08a0ed8c550931448b776af8ae8439bce14d
Jacob Woetmann Kastberg <jacob.w.kastberg@hotmail.com>
2024-04-24 18:24:02 +0200

    Project setup

---
+++ RPG_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RPG_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RPG_GameLogic.GameManagement.Game game = new();
+            game.Start();
+
+        }
+    }
+}
